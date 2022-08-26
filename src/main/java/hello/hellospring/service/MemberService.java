ba828f4dd5920763d23063fb9648504fd0c58070
portuna85 <portuna85@gmail.com>
2022-08-26 22:49:28 +0900

    AOP Update

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -22,11 +22,11 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        // 같은 이름이 있는 중복 회원 X
-        validateDuplicateMember(member);    // 중복 회원 검증
 
+        validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
