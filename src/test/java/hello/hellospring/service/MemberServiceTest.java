2cd28e94471416a81fe3006c515e2b1275710106
portuna85 <portuna85@gmail.com>
2022-08-26 15:01:25 +0900

    Test UPDATE

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("Hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
